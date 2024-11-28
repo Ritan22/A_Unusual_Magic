b71fdb7075f7f7a95f9d3c884e0f610f1d33feef
Leo <leo.r2002@live.it>
2024-11-28 23:46:06 +0100

    inzio minigioco

---
+++ Assets/Scripts/Minigioco.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minigioco : MonoBehaviour
+{
+    public GameObject _1;
+    public GameObject _2;
+    public GameObject _3;
+    public GameObject _4;
+    public GameObject _5;
+    public GameObject _6;
+    public GameObject _7;
+    public GameObject _8;
+    public GameObject _9;
+    public GameObject _10;
+    public GameObject _11;
+    public GameObject _12;
+    public GameObject _13;
+    public GameObject _14;
+    public GameObject _15;
+    public GameObject _16;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
